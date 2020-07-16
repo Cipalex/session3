54979e727e6f2aef125fa99607205eff73086b56
ciprian.chiriac <chiriac.ac@gmail.com>
2020-07-16 19:14:11 +0300

    Added second print line

---
+++ Test_1.py
@@ -0,0 +1,19 @@
+print('Program for homework 1')
+a = [1, 2, 3, 4, 5]
+print('Ex.1 - We have this array: ' + str(a))
+a.reverse()
+print('Reverse of array is below:')
+print(a)
+print('')
+
+a = [1, 1, 2, 2, 2, 2, 3, 3, 3]
+b = 2
+c = a.count(b)
+print('Ex.2 - We have this array: ' + str(a))
+print('We found variable ' + str(b) + ' only ' + str(c) + ' times')
+print('')
+
+a = 'ana are mere si nu are pere'
+print('Ex.3 - We have the text: ' + a )
+words = len(a.split())
+print('This text has ' + str(words) + ' words')
